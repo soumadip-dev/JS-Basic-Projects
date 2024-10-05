1d871b32ccd031207b48facea62d4230726544fb
Soumadip Majila <soumadipmajila@gmail.com>
2024-10-05 14:32:09 +0530

    Update script.js

--- 02-Modal-Window/script.js
+++ 02-Modal-Window/script.js
@@ -1,33 +1,35 @@
-// Select elements from the DOM
-const modal = document.querySelector('.modal');
-const overlay = document.querySelector('.overlay');
-const btnCloseModal = document.querySelector('.close-modal');
-const btnsOpenModal = document.querySelectorAll('.show-modal');
+document.addEventListener('DOMContentLoaded', function () {
+  ////////////// DOM Elements
+  const modal = document.querySelector('.modal');
+  const overlay = document.querySelector('.overlay');
+  const btnCloseModal = document.querySelector('.close-modal');
+  const btnsOpenModal = document.querySelectorAll('.show-modal');
 
-// Function to open the modal
-const openModal = () => {
-  modal.classList.remove('hidden');
-  overlay.classList.remove('hidden');
-};
+  ////////////// Open modal function
+  const openModal = function () {
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+  };
 
-// Add event listeners to open modal buttons
-for (let i = 0; i < btnsOpenModal.length; i++) {
-  btnsOpenModal[i].addEventListener('click', openModal);
-}
+  ////////////// Close modal function
+  const closeModal = function () {
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+  };
 
-// Function to close the modal
-const closeModal = () => {
-  modal.classList.add('hidden');
-  overlay.classList.add('hidden');
-};
-
-// Add event listeners to close modal button and overlay
-btnCloseModal.addEventListener('click', closeModal);
-overlay.addEventListener('click', closeModal);
-
-// Add event listener to document for Escape key press
-document.addEventListener('keydown', function (event) {
-  if (event.key === 'Escape' && !modal.classList.contains('hidden')) {
-    closeModal();
+  ////////////// Add event listeners to open modal buttons
+  for (let i = 0; i < btnsOpenModal.length; i++) {
+    btnsOpenModal[i].addEventListener('click', openModal);
   }
+
+  ////////////// Add event listeners to close modal button and overlay
+  btnCloseModal.addEventListener('click', closeModal);
+  overlay.addEventListener('click', closeModal);
+
+  ////////////// Close modal on Escape key press
+  document.addEventListener('keydown', function (event) {
+    if (event.key === 'Escape' && !modal.classList.contains('hidden')) {
+      closeModal();
+    }
+  });
 });
